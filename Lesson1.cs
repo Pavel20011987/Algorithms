075b379f0ed68445ebbd646fbb7b04665d3190e1
Pavel20011987 <ivanovpaul2008@rambler.ru>
2022-05-18 21:26:19 +0300

    Решение 1 задания

---
+++ Lesson1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    internal class Lesson1
+    {
+        /// <summary>
+        /// Метод решения 1 задания
+        /// </summary>
+        public static void Task1()
+        {
+            //Объявление переменных
+            int number, d, i;
+            string userAnswer;
+
+            Console.WriteLine("Требуется реализовать на C# функцию согласно блок-схеме." +
+                "Блок-схема описывает алгоритм проверки, простое число или нет.\n");
+            while (true)
+            {
+                Console.WriteLine("Введите целое число или exit для выхода:");
+                userAnswer = Console.ReadLine();
+                if (int.TryParse(userAnswer, out number))
+                    break;
+                if (userAnswer.Trim(' ') == "exit")
+                    return;
+            }
+            d = 0;
+            i = 2;
+            while (i < number)
+            {
+                if (number % i == 0)
+                    d++;
+                i++;
+            }
+            if (d == 0)
+                Console.WriteLine($"Число {number} является простым");
+            else
+                Console.WriteLine($"Число {number} не является простым");
+
+        }
+    }
+}
