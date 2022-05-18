56711f86374f759a812ad389beb4d8ddeea31189
Pavel20011987 <ivanovpaul2008@rambler.ru>
2022-05-18 21:34:47 +0300

    Решение 3 задания

--- Lesson1.cs
+++ Lesson1.cs
@@ -50,6 +50,80 @@ namespace Algorithms
             Console.WriteLine("Посчитайте сложность функции");
             Console.WriteLine("Ответ: Сложность алгоритма O(N^3)");
         }
+        /// <summary>
+        /// Метод решения 3 задания
+        /// </summary>
+        public static void Task3()
+        {
+            Console.WriteLine("Введите число число для Фибоначчи: ");
+            int number = Convert.ToInt32(Console.ReadLine());
 
+            int numberFibo = FibonacciRecursion(number);
+
+            Console.WriteLine(numberFibo);
+
+
+        }
+
+        //Метод поиска числа Фибоначчи с помощью рекурсии
+        static int FibonacciRecursion(int number)
+        {
+            if (number == 0)
+                return 0;
+            else if (number == 1)
+                return 1;
+            else
+            {
+                return FibonacciRecursion(number - 2) + FibonacciRecursion(number - 1);
+            }
+        }
+
+        //Метод поиска числа Фибоначчи с помощью цикла
+        static int FibonacciCycle(int number)
+        {
+            int fiboStart = 0;
+            int fiboFinish = 1;
+            int sum;
+
+            if (number == 0)
+                return 0;
+            else if (number == 1)
+                return 1;
+            else
+            {
+                for (int i = 2; i <= number; i++)
+                {
+                    sum = fiboStart + fiboFinish;
+                    fiboStart = fiboFinish;
+                    fiboFinish = sum;
+                }
+                return fiboFinish;
+            }
+
+        }
+        /// <summary>
+        /// Реализация метода, объединяющего все решения заданий
+        /// </summary>
+        public static void RunLesson1()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите номер задачи (1-3) или 0 для выхода:");
+                if (!int.TryParse(Console.ReadLine(), out int taskNum) || taskNum < 0 || taskNum > 3)
+                    continue;
+                if (taskNum == 0)
+                    break;
+                if (taskNum == 1)
+                    Task1();
+                else if (taskNum == 2)
+                    Task2();
+                else if (taskNum == 3)
+                    Task3();
+
+                Console.WriteLine();
+            }
+        }
     }
+
 }
+
