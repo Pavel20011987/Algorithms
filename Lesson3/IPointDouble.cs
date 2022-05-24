42d3ca2b143857e16272d6b598ab8b1e10671da7
Pavel20011987 <ivanovpaul2008@rambler.ru>
2022-05-25 01:28:28 +0300

    Решение 3 задания

---
+++ Lesson3/IPointDouble.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Lesson3
+{
+    internal interface IPointDouble
+    {
+        double X1 { get; }
+        double Y1 { get; }
+        double X2 { get; }
+        double Y2 { get; }
+    }
+}
